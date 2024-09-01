f0f8fb5d95fdfa030d0903abac8060a907484ebd
Shreyas Waichal <88624305+shreyaswaichal1989@users.noreply.github.com>
2024-09-01 18:46:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,12 @@
+resource "random_id" "random-vm-name" {
+ 
+  byte_length = 8
+}
+
+
 resource "google_compute_instance" "vm-cloud-assesments" {
-  name="vm-cloud-assesments"
+  #name="vm-cloud-assesments"
+  name=random_id.random-vm-name.hex
   machine_type = "e2-medium"
   zone="us-central1-a"
 
