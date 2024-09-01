df688dd54f79426ab5ecbe8ab3e6ad6ee12097b0
Shreyas Waichal <88624305+shreyaswaichal1989@users.noreply.github.com>
2024-09-01 19:00:27 +0530

    reverted the Changes

--- main.tf
+++ main.tf
@@ -1,20 +1,5 @@
-resource "random_string" "random-vm-name" {
- 
- length  = 9
-  upper   = false
-  #number  = false
-  lower   = true
-  special = false
-}
-
-locals {
-  vm-name = "${random_string.random-vm-name}-cloudsolutions"
-}
-
-
 resource "google_compute_instance" "vm-cloud-assesments" {
   name="vm-cloud-assesments"
-  #name=local.vm-name
   machine_type = "e2-medium"
   zone="us-central1-a"
 
