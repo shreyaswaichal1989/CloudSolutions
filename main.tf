fa814ad188f30c21806d1c54fcd2f16a17ad0746
Shreyas Waichal <88624305+shreyaswaichal1989@users.noreply.github.com>
2024-09-01 18:57:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,20 @@
-resource "random_id" "random-vm-name" {
+resource "random_string" "random-vm-name" {
  
-  byte_length = 8
+ length  = 9
+  upper   = false
+  #number  = false
+  lower   = true
+  special = false
+}
+
+locals {
+  vm-name = "${random_string.random-vm-name}-cloudsolutions"
 }
 
 
 resource "google_compute_instance" "vm-cloud-assesments" {
   #name="vm-cloud-assesments"
-  name="$cloud-solutions- $random_id.random-vm-name.hex"
+  name=local.vm-name
   machine_type = "e2-medium"
   zone="us-central1-a"
 
